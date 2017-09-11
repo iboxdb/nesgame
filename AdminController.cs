330011a6b8d30b300f9ef74dacf8ca0bdf349d84
Fast transactional nosql Application Database <iboxdb@users.noreply.github.com>
2017-09-11 18:02:40 +0800

    remove limit

---
+++ AdminController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game.Controllers
+{
+    using Game.Models;
+
+    [Route("api/[controller]")]
+    public class AdminController : Controller
+    {
+        [HttpGet("Ver")]
+        public long Ver()
+        {
+            return App.Auto.NewId(byte.MaxValue, 0);
+        }
+
+        [HttpGet("{last}")]
+        public IEnumerable<User> Get(long last)
+        {
+            if (last == 0)
+            {
+                DateTime dt = DateTime.Now;
+                dt = dt.Subtract(TimeSpan.FromSeconds(60 * 1));
+                return App.Auto.Select<User>("from User where Ver>? & Time>?", last, dt);
+            }
+            else
+            {
+                return App.Auto.Select<User>("from User where Ver>?", last);
+            }
+        }
+    }
+}
